bc3acc5607672836cc17a1269f114a35854a2c19
Emmanuel CLAIRE <113002414+Emmanuel972@users.noreply.github.com>
2022-11-21 20:29:17 +0100

    Revert "update devise"

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  #resources :steps, only: [:show, :new, :create] do
-  #  resouces :user_steps
-  #end
+  resources :steps, only: [:show, :new, :create] do
+    resouces :user_steps
+  end
 end
