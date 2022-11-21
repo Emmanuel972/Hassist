55faf56b0e7d80d9f0249fcd3598cf08e38b2479
Emmanuel CLAIRE <c.l.e.972@gmail.com>
2022-11-21 19:57:37 +0100

    add validations
    Signed-off-by: Emmanuel CLAIRE <c.l.e.972@gmail.com>

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,7 @@
 class Step < ApplicationRecord
+  has_many :user_steps
+
+  validates :name, :content, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :content, length: { minimum: 10 }
 end
