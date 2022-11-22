09b8420805729d5a36253452c1b15a401cb32de4
Lucie Jesoph <matjay972@outlook.fr>
2022-11-22 11:26:44 -0400

    translate
    Signed-off-by: Lucie Jesoph <matjay972@outlook.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   #resources :steps, only: [:show, :new, :create] do
-  #  resouces :user_steps
+  #  resources :user_steps
   #end
 end
