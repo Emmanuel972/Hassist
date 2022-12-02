fa7f233fb2fbd3b23d9c7891dfbd7f825bf5e900
Emmanuel CLAIRE <c.l.e.972@gmail.com>
2022-12-02 03:17:21 +0100

    ...
    Signed-off-by: Emmanuel CLAIRE <c.l.e.972@gmail.com>

--- app/models/step.rb
+++ app/models/step.rb
@@ -14,14 +14,14 @@ class Step < ApplicationRecord
     case name
     when "MDPH"
       return [{ name: "Demande à la MDPH",
-                url: "https://res.cloudinary.com/daixhq5r5/image/upload/v1669835428/development/demande_%C3%A0_la_MDPH_neo9by.pdf",
-                key: "demande_à_la_MDPH_neo9by" },
+                url: "https://asset.cloudinary.com/daixhq5r5/1af939d44835060c05f0adf0fa203eda",
+                key: "demande_à_la_MDPH_f6pmsj" },
               { name: "Certificat médical",
-                url:"https://res.cloudinary.com/daixhq5r5/image/upload/v1669835438/development/certificat_m%C3%A9dical_vj8qrr.pdf",
-                key: "certificat_médical_vj8qrr" },
+                url: "https://asset.cloudinary.com/daixhq5r5/e7bd8fe204a2ea7f24769f1b5ebb62f7",
+                key: "certificat_médical_syzwo0" },
               { name: "Geva-sco",
-                url: "https://res.cloudinary.com/daixhq5r5/image/upload/v1669835453/development/Geva-sco_t3ao7b.pdf",
-                key: "Geva-sco_t3ao7b" }]
+                url: "https://asset.cloudinary.com/daixhq5r5/fd52db4562504c861c118389ffb64694",
+                key: "Geva-sco_sjyqul" }]
     end
   end
 end
